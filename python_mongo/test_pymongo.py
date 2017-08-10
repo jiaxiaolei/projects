e1d2913aa16ac9541a540cd5a757b4e8427a5df0
jiaxiaolei <jiaxiaolei@syswin.com>
2017-08-10 09:33:46 +0000

    chore: beauty the code

--- python_mongo/test_pymongo.py
+++ python_mongo/test_pymongo.py
@@ -2,13 +2,15 @@
 # coding=utf-8
 
 from pymongo import MongoClient
-#c = MongoClient(host="mongo-1", port=28010)
+
+# single mongo
+#c = MongoClient(host="mongo-1", port=28010) # okay
 #c = MongoClient('mongodb://admin:123456@mongo-1:28010,mongo-2:28010/?replicaSet=rsname')
-# okay
+
+# mongo cluster 
 c = MongoClient('mongodb://admin:123456@mongo-1:28010,mongo-2:28010,mongo-3:28010')
 #c = MongoClient('mongodb://admin:123456@mongo-1:28010,mongo-2:28010,mongo-3:28010/?readPreference=secondary')
 #c = MongoClient('mongodb://admin:123456@mongo-3:28010/?readPreference=secondary')
-print c
 
 print c.nodes
 print c.database_names()
